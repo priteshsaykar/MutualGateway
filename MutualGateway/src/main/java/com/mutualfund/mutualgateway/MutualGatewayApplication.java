880399811523f8bb54d148e971c00507c621f561
pritesh saykar <pritesh.saykar413@gmail.com>
2024-12-07 18:06:06 +0530

    add the gateway project

---
+++ MutualGateway/src/main/java/com/mutualfund/mutualgateway/MutualGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.mutualfund.mutualgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MutualGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualGatewayApplication.class, args);
+	}
+
+}
---
+++ MutualGateway/src/test/java/com/mutualfund/mutualgateway/MutualGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.mutualfund.mutualgateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MutualGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
